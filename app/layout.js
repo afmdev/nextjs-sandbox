af5ae9a3a383cd0dcba3d4b693a9110224105b79
Alejandro FM <102042032+alejanfm@users.noreply.github.com>
2023-04-26 20:11:33 +0200

    Installing Tailwind CSS to the project

--- app/layout.js
+++ app/layout.js
@@ -1,34 +1,13 @@
-import Link from "next/link"
-
-const links = [{
-	label: 'Home',
-	route: '/'
-},
-{
-	label: 'About',
-	route: '/about'
-}]
+import { Navigation } from "./components/Navigation"
 
 export default function RootLayout({ children }) {
 	return (
 		<html>
 			<head>
-				<titlte>My app with Next.js 13</titlte>
+				<title>My app with Next.js 13</title>
 			</head>
 			<body>
-				<header>
-					<nav>
-						<ul>
-							{links.map(({ label, route }) => (
-								<li key={route}>
-									<Link href={route}>
-										<a>{label}</a>
-									</Link>
-								</li>
-							))}
-						</ul>
-					</nav>
-				</header>
+				<Navigation />
 				{children}
 			</body>
 		</html>
