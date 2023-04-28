89c60c2a5ea04fbc71c5e0ff2c7fdef4675ab9e3
Alejandro FM <102042032+alejanfm@users.noreply.github.com>
2023-04-28 10:17:57 +0200

    Config contentful fetch

--- app/page.js
+++ app/page.js
@@ -1,6 +1,27 @@
-export default function HomePage() {
+import { createClient } from "contentful"
+
+export default function HomePage(props) {
 	return (
-		<h1>This is my App</h1>
+		<div>
+			<h1>This is my App</h1>
+			{console.log("Posts", props.posts)}
+		</div>
 	)
 
+}
+
+//Exporting the function cgetStaticProps will pre-render the page at build time using the props returned from the function
+export async function getStaticProps() {
+	const client = createClient({
+		space: process.env.CONTENTFUL_SPACE_ID,
+		accessToken: process.env.CONTENTFUL_ACCESS_KEY
+	})
+
+	const res = await client.getEntries({ content_type: 'Post' })
+
+	return {
+		props: {
+			posts: res
+		}
+	}
 }
