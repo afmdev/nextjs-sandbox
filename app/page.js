c90d6997b8ca22a25fad0dc62d7c4c4f97c7b32a
Alejandro Marrero <crixto@gmail.com>
2023-04-29 19:02:00 +0200

    FIxed images and AI model

--- app/page.js
+++ app/page.js
@@ -1,15 +1,19 @@
-import { getPost, getModel } from '../utils/API';
+import { getPost, getModel, getCategory } from '../utils/API';
 
 export default async function HomePage() {
 
 	const posts = await getPost();
 	const model = await getModel();
-	
+	const category = await getCategory();
+
 	const blogPosts = posts.items
+	const blogCoverImage = posts.includes.Asset
 	const blogModel = model.items
+	const blogCategory = category.items
 
 	return (
 		<div div className="relative isolate pt-10">
+
 			<div
 				className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
 				aria-hidden="true"
@@ -60,17 +64,20 @@ export default async function HomePage() {
 							<div key={post.sys.id} className="group relative">
 								<div className="card-content ring-0 rounded-lg flex flex-col space-y-2 p-4 break-inside-avoid mb-6 bg-white transform duration-200 hover:shadow-gray-200 hover:shadow-md  z-0 relative shadow-2xl hover:scale-[101%]">
 									<div className="card-image overflow-hidden rounded-lg">
-										<a href="">
-											<img src="" alt="" className="hover:scale-[103%] transform duration-200" />
+										<a href={`/ai/${post.fields.slug}`}>
+											<img src={blogCoverImage.find(img => img.sys.id === post.fields.coverImage.sys.id).fields.file.url} alt="" className="hover:scale-[103%] transform duration-200" />
 										</a>
 									</div>
-									<div className="card-title flex justify-center text-xl font-black">
-										<a href="">
+									<div className="card-title text-lg font-bold pt-3">
+										<a href={`/ai/${post.fields.slug}`}>
 											<h3>{post.fields.title}</h3>
 										</a>
+										<span className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 mt-2 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+											YOYO
+										</span>
 									</div>
 									<div className="card-description">
-										<p className="overflow-hidden line-clamp-3 mt-4">{post.fields.content}</p>
+										<p className="overflow-hidden line-clamp-3 mt-2">{post.fields.content}</p>
 									</div>
 									<div className="flex flex-wrap justify-between items-center border-t pt-2 flex-col md:flex-row">
 										<div className="flex space-x-2">
@@ -91,7 +98,7 @@ export default async function HomePage() {
 												</svg>
 												<span className="">Visit</span>
 											</a>
-											<a href={`${post.fields.slug}`} className="border border-gray-300 rounded-full px-4 p-2 hover:bg-gray-100 hover:border-gray-200 hover:text-gray-900 cursor-pointer bg-white text-sm flex items-center ml-1 mt-1">
+											<a href={`/ai/${post.fields.slug}`} className="border border-gray-300 rounded-full px-4 p-2 hover:bg-gray-100 hover:border-gray-200 hover:text-gray-900 cursor-pointer bg-white text-sm flex items-center ml-1 mt-1">
 												<span className="">Details</span>
 											</a>
 										</div>
