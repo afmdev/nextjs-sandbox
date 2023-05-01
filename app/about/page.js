631512924896008527de1a7963b9c9e16f4f5b75
Alejandro FM <102042032+alejanfm@users.noreply.github.com>
2023-05-01 21:56:31 +0200

    Implemented single pages for each blogpost

--- app/about/page.js
+++ app/about/page.js
@@ -27,34 +27,20 @@ const features = [
 	},
 ]
 
-const categories = [
-	{ id: 1, name: 'Category 1' },
-	{ id: 2, name: 'Category 2' },
-	{ id: 3, name: 'Category 3' }
-];
-
-const products = [
-	{ id: 1, name: 'Product 1', category_id: 1 },
-	{ id: 2, name: 'Product 2', category_id: 2 },
-	{ id: 3, name: 'Product 3', category_id: 3 }
-];
-
 export default function About() {
 	return (
-		<div className="bg-white py-24 sm:py-32">
-
-			<ul>
-				{products.map(product => (
-					<li key={product.id}>
-						<strong>{product.name}</strong> -
-
-						{categories.find(category => category.id === product.category_id).name}
-
-
-					</li>
-				))}
-			</ul>
-
+		<div className="relative isolate pt-10 py-24 sm:py-32">
+			<div
+				className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+				aria-hidden="true"
+			>
+				<div
+					className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+					style={{
+						clipPath:
+							'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+					}} />
+			</div>
 
 			<div className="mx-auto max-w-7xl px-6 lg:px-8">
 				<div className="mx-auto max-w-2xl lg:text-center">
--- app/page.js
+++ app/page.js
@@ -46,22 +46,12 @@ export default async function HomePage() {
 					</div>
 				</div>
 			</div>
-			<div
-				className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-				aria-hidden="true">
-				<div
-					className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-					style={{
-						clipPath:
-							'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-					}} />
-			</div>
 			<div className="bg-white">
 				<div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
 
 					<div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-6">
 
-						{blogPosts.map((post, i) => (
+						{blogPosts.slice(0,3).map((post, i) => (
 							<div key={post.sys.id} className="group relative">
 								<div className="card-content ring-0 rounded-lg flex flex-col space-y-2 p-4 break-inside-avoid mb-6 bg-white transform duration-200 hover:shadow-gray-200 hover:shadow-md  z-0 relative shadow-2xl hover:scale-[101%]">
 									<div className="card-image overflow-hidden rounded-lg">
@@ -81,18 +71,24 @@ export default async function HomePage() {
 													{blogCategory.find(catName => catName.sys.id === category.sys.id).fields.name}
 												</div>
 											))}
-
 										</span>
 									</div>
 									<div className="card-description">
 										<p className="overflow-hidden line-clamp-3 mt-2">{post.fields.content}</p>
+										<div>
+											{post.fields.tagss.map((tag) => (
+												<div key={tag.sys.id} className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-normal  text-gray-600 ring-1 ring-inset ring-gray-500/10 mt-2 mr-2">
+													#{blogTag.find(tagName => tagName.sys.id === tag.sys.id).fields.name}
+												</div>
+											))}
+										</div>
 									</div>
 									<div className="flex flex-wrap justify-between items-center border-t pt-2 flex-col md:flex-row">
 										<div className="flex space-x-2">
 											<div className="text-sm rounded-full px-4 p-2 capitalize bg-white text-gray-800 border border-gray-400">
 
 
-												{blogPaidFree.find(model => model.sys.id === post.fields.model.sys.id).fields.name}
+												{blogPaidFree.find(paidFree => paidFree.sys.id === post.fields.model.sys.id).fields.name}
 
 
 											</div>
