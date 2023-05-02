ef9047ccb2623d663a2e9186e1cf2aaddc1b3843
Alejandro FM <102042032+alejanfm@users.noreply.github.com>
2023-05-02 16:50:04 +0200

    Fixing eslint errors

--- app/ai/[slug]/page.js
+++ app/ai/[slug]/page.js
@@ -1,13 +1,12 @@
 export default function AiPageSingle({ params }) {
-
-  const { slug } = params
+  const { slug } = params;
 
   return (
     <div className="w-full dark:bg-gray-800 overflow-hidden">
+      {console.log(slug)}
 
       <div className="container mx-auto max-w-7xl">
         <div className="mt-22 px-4 sm:px-0">
-
           <div>
             <div className="flex space-x-2 mb-5 items-center">
               <a href="/">Home</a>
@@ -19,17 +18,14 @@ export default function AiPageSingle({ params }) {
           </div>
           <div className="flex justify-between">
             <h1 className="text-2xl font-semibold block">AI Backdrop</h1>
-
           </div>
           <div className="flex flex-col md:flex-row md:space-x-10 mt-5">
             <div className="md:w-2/6">
               <img src="" alt="AI Backdrop" className="rounded-xl border" />
               <div className="flex items-center justify-between mt-6">
-                <div className="border border-gray-300 px-4 py-2 rounded-full bg-white">free
-                </div>
+                <div className="border border-gray-300 px-4 py-2 rounded-full bg-white">free</div>
                 <div className="flex items-center">
                   <a href="#" target="_blank" title="AI Backdrop" className="border border-gray-300 rounded-full px-6 p-2 hover:bg-gray-100 hover:border-gray-200 hover:text-gray-900 cursor-pointer text-white bg-blue-500 text-lg flex items-center space-x-2">
-
                     <div className="">Visit</div>
                   </a>
                 </div>
@@ -59,7 +55,5 @@ export default function AiPageSingle({ params }) {
         </div>
       </div>
     </div>
-
-
-  )
+  );
 }
