711aff36621d45372bf961d3548ab9866fb1bdd6
Alejandro FM <102042032+alejanfm@users.noreply.github.com>
2023-05-02 14:54:58 +0200

    Fixing spaces indentation to deploy in vercel

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,29 @@
-import "../styles/globals.css"
+import "../styles/globals.css";
 
-import { Navigation } from "../components/Navigation"
-import { TopBar } from "../components/TopBar"
-import { Footer } from "../components/Footer"
+import { Navigation } from "../components/Navigation";
+import { TopBar } from "../components/TopBar";
+import { Footer } from "../components/Footer";
 
 export default function RootLayout({ children }) {
-	return (
-		<html>
-			<head>
-				<title>AI TOOLS LISTING</title>
-			</head>
-			<body>
-				<header>
-					<div>
-						<TopBar />
-					</div>
-					<Navigation />
-				</header>
+  return (
+    <html>
+      <head>
+        <title>AI TOOLS LISTING</title>
+      </head>
+      <body>
+        <header>
+          <div>
+            <TopBar />
+          </div>
+          <Navigation />
+        </header>
 
-				{children}
+        {children}
 
-				<footer>
-					<Footer />
-				</footer>
-			</body>
-		</html>
-	)
+        <footer>
+          <Footer />
+        </footer>
+      </body>
+    </html>
+  );
 }
