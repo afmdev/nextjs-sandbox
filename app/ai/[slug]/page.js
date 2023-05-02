ab86031e94ae00c8f07f7842535b4b9ce4de4f1b
Alejandro Marrero <crixto@gmail.com>
2023-05-02 14:54:58 +0200

    Fixing spaces indentation to deploy in vercel

--- app/ai/[slug]/page.js
+++ app/ai/[slug]/page.js
@@ -1,65 +1,65 @@
 export default function AiPageSingle({ params }) {
 
-	const { slug } = params
+  const { slug } = params
 
-	return (
-		<div className="w-full dark:bg-gray-800 overflow-hidden">
+  return (
+    <div className="w-full dark:bg-gray-800 overflow-hidden">
 
-			<div className="container mx-auto max-w-7xl">
-				<div className="mt-22 px-4 sm:px-0">
+      <div className="container mx-auto max-w-7xl">
+        <div className="mt-22 px-4 sm:px-0">
 
-					<div>
-						<div className="flex space-x-2 mb-5 items-center">
-							<a href="/">Home</a>
+          <div>
+            <div className="flex space-x-2 mb-5 items-center">
+              <a href="/">Home</a>
 
-							<a href="/ai-sites/art">Art</a>
+              <a href="/ai-sites/art">Art</a>
 
-							<span>AI Backdrop</span>
-						</div>
-					</div>
-					<div className="flex justify-between">
-						<h1 className="text-2xl font-semibold block">AI Backdrop</h1>
+              <span>AI Backdrop</span>
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <h1 className="text-2xl font-semibold block">AI Backdrop</h1>
 
-					</div>
-					<div className="flex flex-col md:flex-row md:space-x-10 mt-5">
-						<div className="md:w-2/6">
-							<img src="" alt="AI Backdrop" className="rounded-xl border" />
-							<div className="flex items-center justify-between mt-6">
-								<div className="border border-gray-300 px-4 py-2 rounded-full bg-white">free
-								</div>
-								<div className="flex items-center">
-									<a href="#" target="_blank" title="AI Backdrop" className="border border-gray-300 rounded-full px-6 p-2 hover:bg-gray-100 hover:border-gray-200 hover:text-gray-900 cursor-pointer text-white bg-blue-500 text-lg flex items-center space-x-2">
+          </div>
+          <div className="flex flex-col md:flex-row md:space-x-10 mt-5">
+            <div className="md:w-2/6">
+              <img src="" alt="AI Backdrop" className="rounded-xl border" />
+              <div className="flex items-center justify-between mt-6">
+                <div className="border border-gray-300 px-4 py-2 rounded-full bg-white">free
+                </div>
+                <div className="flex items-center">
+                  <a href="#" target="_blank" title="AI Backdrop" className="border border-gray-300 rounded-full px-6 p-2 hover:bg-gray-100 hover:border-gray-200 hover:text-gray-900 cursor-pointer text-white bg-blue-500 text-lg flex items-center space-x-2">
 
-										<div className="">Visit</div>
-									</a>
-								</div>
-							</div>
-						</div>
-						<div className="md:w-4/6 pt-6 md:pt-0">
-							<h2 className="block mb-5">Demo text</h2>
-							<div className="flex space-x-3 mt-6 flex-wrap">
-								<a href="/tag/ai" className="rounded-full border-gray-300 border px-4 text-sm py-1 mt-2 md:mt-0">
-									AI
-								</a>
-								<a href="/tag/social-media" className="rounded-full border-gray-300 border px-4 text-sm py-1 mt-2 md:mt-0">
-									Social media
-								</a>
-								<a href="/tag/marketing" className="rounded-full border-gray-300 border px-4 text-sm py-1 mt-2 md:mt-0">
-									Marketing
-								</a>
-								<a href="/tag/product-photography" className="rounded-full border-gray-300 border px-4 text-sm py-1 mt-2 md:mt-0">
-									Product photography
-								</a>
-								<a href="/tag/e-commerce" className="rounded-full border-gray-300 border px-4 text-sm py-1 mt-2 md:mt-0">
-									E-commerce
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
+                    <div className="">Visit</div>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="md:w-4/6 pt-6 md:pt-0">
+              <h2 className="block mb-5">Demo text</h2>
+              <div className="flex space-x-3 mt-6 flex-wrap">
+                <a href="/tag/ai" className="rounded-full border-gray-300 border px-4 text-sm py-1 mt-2 md:mt-0">
+                  AI
+                </a>
+                <a href="/tag/social-media" className="rounded-full border-gray-300 border px-4 text-sm py-1 mt-2 md:mt-0">
+                  Social media
+                </a>
+                <a href="/tag/marketing" className="rounded-full border-gray-300 border px-4 text-sm py-1 mt-2 md:mt-0">
+                  Marketing
+                </a>
+                <a href="/tag/product-photography" className="rounded-full border-gray-300 border px-4 text-sm py-1 mt-2 md:mt-0">
+                  Product photography
+                </a>
+                <a href="/tag/e-commerce" className="rounded-full border-gray-300 border px-4 text-sm py-1 mt-2 md:mt-0">
+                  E-commerce
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
 
-	)
+  )
 }
--- components/Navigation.js
+++ components/Navigation.js
@@ -1,44 +1,39 @@
 "use client";
 
-import Link from "next/link"
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Link from "next/link";
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
-const navigation = [{
-	name: 'Home',
-	href: '/'
-}, {
-	name: 'AI Tools',
-	href: '/ai'
-}, {
-	name: 'About',
-	href: '/about'
-}]
+const navigation = [
+	{
+		name: "Home",
+		href: "/",
+	},
+	{
+		name: "AI Tools",
+		href: "/ai",
+	},
+	{
+		name: "About",
+		href: "/about",
+	},
+];
 
 export function Navigation() {
-	const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+	const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
 	return (
-
 		<div className="absolute inset-x-0 z-50 bg-white">
 			<nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
 				<div className="flex lg:flex-1">
 					<a href="#" className="-m-1.5 p-1.5">
 						<span className="sr-only">Your Company</span>
-						<img
-							className="h-8 w-auto"
-							src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-							alt=""
-						/>
+						<img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
 					</a>
 				</div>
 				<div className="flex lg:hidden">
-					<button
-						type="button"
-						className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-						onClick={() => setMobileMenuOpen(true)}
-					>
+					<button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" onClick={() => setMobileMenuOpen(true)}>
 						<span className="sr-only">Open main menu</span>
 						<Bars3Icon className="h-6 w-6" />
 					</button>
@@ -46,10 +41,7 @@ export function Navigation() {
 				<div className="hidden lg:flex lg:gap-x-12">
 					{navigation.map(({ name, href }) => (
 						<div key={href}>
-							<Link
-								href={href}
-								className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-							>
+							<Link href={href} className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
 								{name}
 							</Link>
 						</div>
@@ -68,17 +60,9 @@ export function Navigation() {
 					<div className="flex items-center justify-between">
 						<a href="#" className="-m-1.5 p-1.5">
 							<span className="sr-only">Your Company</span>
-							<img
-								className="h-8 w-auto"
-								src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-								alt=""
-							/>
+							<img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
 						</a>
-						<button
-							type="button"
-							className="-m-2.5 rounded-md p-2.5 text-gray-700"
-							onClick={() => setMobileMenuOpen(false)}
-						>
+						<button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700" onClick={() => setMobileMenuOpen(false)}>
 							<span className="sr-only">Close menu</span>
 							<XMarkIcon className="h-6 w-6" />
 						</button>
@@ -86,13 +70,9 @@ export function Navigation() {
 					<div className="mt-6 flow-root">
 						<div className="-my-6 divide-y divide-gray-500/10">
 							<div className="space-y-2 py-6">
-
 								{navigation.map(({ name, href }) => (
 									<div key={href}>
-										<Link
-											href={href}
-											className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-										>
+										<Link href={href} className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
 											{name}
 										</Link>
 									</div>
@@ -103,5 +83,5 @@ export function Navigation() {
 				</Dialog.Panel>
 			</Dialog>
 		</div>
-	)
+	);
 }
