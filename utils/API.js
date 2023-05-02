421c43a8de336d64f52dd1d2b672442da1288921
Alejandro FM <102042032+alejanfm@users.noreply.github.com>
2023-05-02 21:02:43 +0200

    Added tool single page

--- utils/API.js
+++ utils/API.js
@@ -2,11 +2,11 @@ export async function getPost() {
 	try {
 		const response = await fetch(
 			`https://cdn.contentful.com/spaces/39eo3nd1n1px/entries?access_token=Kg2zFy4PmKUAwMZKHN3vy7ZrB8AM1G3weQdFBSJkP0c&content_type=post`
-		);
-		const data = await response.json();
-		return data;
+		)
+		const data = await response.json()
+		return data
 	} catch (error) {
-		console.log(error);
+		console.log(error)
 	}
 }
 
@@ -14,11 +14,11 @@ export async function getCategory() {
 	try {
 		const response = await fetch(
 			`https://cdn.contentful.com/spaces/39eo3nd1n1px/entries?access_token=Kg2zFy4PmKUAwMZKHN3vy7ZrB8AM1G3weQdFBSJkP0c&content_type=categories`
-		);
-		const data = await response.json();
-		return data;
+		)
+		const data = await response.json()
+		return data
 	} catch (error) {
-		console.log(error);
+		console.log(error)
 	}
 }
 
@@ -26,11 +26,11 @@ export async function getTag() {
 	try {
 		const response = await fetch(
 			`https://cdn.contentful.com/spaces/39eo3nd1n1px/entries?access_token=Kg2zFy4PmKUAwMZKHN3vy7ZrB8AM1G3weQdFBSJkP0c&content_type=tags`
-		);
-		const data = await response.json();
-		return data;
+		)
+		const data = await response.json()
+		return data
 	} catch (error) {
-		console.log(error);
+		console.log(error)
 	}
 }
 
@@ -38,10 +38,10 @@ export async function getPaidFree() {
 	try {
 		const response = await fetch(
 			`https://cdn.contentful.com/spaces/39eo3nd1n1px/entries?access_token=Kg2zFy4PmKUAwMZKHN3vy7ZrB8AM1G3weQdFBSJkP0c&content_type=blogModel`
-		);
-		const data = await response.json();
-		return data;
+		)
+		const data = await response.json()
+		return data
 	} catch (error) {
-		console.log(error);
+		console.log(error)
 	}
 }
